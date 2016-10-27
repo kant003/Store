af0f75aa7eae7959a8e8da255751cdc4927fe881
Pablo Palacin <pablopalacinmartinez@gmail.com>
2016-10-27 12:17:09 +0200

    Añado ProviderInterface.java

---
+++ src/com/cebem/view/ProviderInteface.java
@@ -0,0 +1,217 @@
+package com.cebem.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JSplitPane;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ProviderInteface {
+
+	private JFrame frame;
+	private JTable tableProvider;
+	private JTextField textField;
+	private JTextField txtName;
+	private JTextField txtId;
+	private JTextField txtAddress;
+	private JTextField txtEmail;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProviderInteface window = new ProviderInteface();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ProviderInteface() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, "name_8487441756629");
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		tableProvider = new JTable();
+		panel.add(tableProvider, BorderLayout.CENTER);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.SOUTH);
+		
+		JButton buttonDelete = new JButton("Borrar Selec.");
+		panel_2.add(buttonDelete);
+		
+		JButton buttonUpdate = new JButton("Editar Selec.");
+		panel_2.add(buttonUpdate);
+		
+		JButton buttonAdd = new JButton("A\u00F1adir Nuevo");
+		panel_2.add(buttonAdd);
+		
+		JButton buttonRefresh = new JButton("Refrescar");
+		panel_2.add(buttonRefresh);
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3, BorderLayout.NORTH);
+		panel_3.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+		
+		JLabel lblNewLabel = new JLabel("Listado de Proveedores");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		panel_3.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		
+		JLabel labelNull = new JLabel("                   ");
+		panel_3.add(labelNull);
+		
+		JLabel lblNewLabel_1 = new JLabel("Buscar :");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		panel_3.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		panel_3.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, "name_8489658499288");
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblEd = new JLabel("Edicion de proveedor");
+		lblEd.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEd.setVerticalAlignment(SwingConstants.TOP);
+		lblEd.setFont(new Font("Tahoma", Font.BOLD, 16));
+		panel_1.add(lblEd, BorderLayout.NORTH);
+		
+		JPanel panel_5 = new JPanel();
+		panel_1.add(panel_5, BorderLayout.SOUTH);
+		panel_5.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		
+		JButton SaveButton = new JButton("Guardar");
+		SaveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_5.add(SaveButton);
+		
+		JButton RefreshButton = new JButton("Actualizar");
+		panel_5.add(RefreshButton);
+		
+		JPanel panel_4 = new JPanel();
+		panel_1.add(panel_4);
+		GridBagLayout gbl_panel_4 = new GridBagLayout();
+		gbl_panel_4.columnWidths = new int[]{166, 222, 0};
+		gbl_panel_4.rowHeights = new int[]{25, 0, 0, 0, 0, 0};
+		gbl_panel_4.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_4.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		panel_4.setLayout(gbl_panel_4);
+		
+		JLabel lblNewLabel_2 = new JLabel("ID");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 0;
+		panel_4.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		txtId = new JTextField();
+		txtId.setEnabled(false);
+		txtId.setEditable(false);
+		GridBagConstraints gbc_txtId = new GridBagConstraints();
+		gbc_txtId.insets = new Insets(0, 0, 5, 0);
+		gbc_txtId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtId.gridx = 1;
+		gbc_txtId.gridy = 0;
+		panel_4.add(txtId, gbc_txtId);
+		txtId.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Nombre");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 1;
+		panel_4.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		txtName = new JTextField();
+		GridBagConstraints gbc_txtName = new GridBagConstraints();
+		gbc_txtName.insets = new Insets(0, 0, 5, 0);
+		gbc_txtName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtName.gridx = 1;
+		gbc_txtName.gridy = 1;
+		panel_4.add(txtName, gbc_txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblAddress = new JLabel("Address");
+		lblAddress.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblAddress = new GridBagConstraints();
+		gbc_lblAddress.anchor = GridBagConstraints.EAST;
+		gbc_lblAddress.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAddress.gridx = 0;
+		gbc_lblAddress.gridy = 2;
+		panel_4.add(lblAddress, gbc_lblAddress);
+		
+		txtAddress = new JTextField();
+		GridBagConstraints gbc_txtAddress = new GridBagConstraints();
+		gbc_txtAddress.insets = new Insets(0, 0, 5, 0);
+		gbc_txtAddress.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtAddress.gridx = 1;
+		gbc_txtAddress.gridy = 2;
+		panel_4.add(txtAddress, gbc_txtAddress);
+		txtAddress.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
+		gbc_lblEmail.anchor = GridBagConstraints.EAST;
+		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEmail.gridx = 0;
+		gbc_lblEmail.gridy = 3;
+		panel_4.add(lblEmail, gbc_lblEmail);
+		
+		txtEmail = new JTextField();
+		GridBagConstraints gbc_txtEmail = new GridBagConstraints();
+		gbc_txtEmail.insets = new Insets(0, 0, 5, 0);
+		gbc_txtEmail.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtEmail.gridx = 1;
+		gbc_txtEmail.gridy = 3;
+		panel_4.add(txtEmail, gbc_txtEmail);
+		txtEmail.setColumns(10);
+	}
+
+}
