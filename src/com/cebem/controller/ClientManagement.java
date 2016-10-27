e74cd30ca172ed017c86a935e50a09ae18507d9e
Valenzu7 <Yapelas@gmail.com>
2016-10-27 12:04:14 +0200

    Añadi métodos para crear una conexion a la base de datos

--- src/com/cebem/controller/ClientManagement.java
+++ src/com/cebem/controller/ClientManagement.java
@@ -1,5 +1,28 @@
 package com.cebem.controller;
 
-public class ClientManagement {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ClientManagement{
+	
+	public static Connection getConnectionDB() throws SQLException{
+		//Creamos la conexión con la base
+		String sURL="jdbc:mysql://10.100.13.110/store_development?useSSL=false";
+		String user="Store";//Con el usuario
+		String pass="Ad123";//Su contraseña
+		String sDriver="com.mysql.jdbc.Driver";//Es necesario el conector correspondiente, en este caso el mysql-connector-java-5.1.39
+		Connection con=null;
+		
+		try{
+			Class.forName(sDriver).newInstance();
+			con=DriverManager.getConnection(sURL,user,pass);
+		}catch(InstantiationException | IllegalAccessException | ClassNotFoundException mte){
+			mte.printStackTrace();
+			throw new SQLException(mte.getMessage());
+		}catch(SQLException e){e.printStackTrace();throw new SQLException(e.getMessage());}
+			
+		return con;
+	}
 
 }
