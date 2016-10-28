c0512a789cb9cb0c6a8fe12be213c70bc037249b
DAM <antvelo.avg@gmail.com>
2016-10-28 11:27:50 +0200

    Fix ClientManagement

--- src/com/cebem/controller/ClientManagement.java
+++ src/com/cebem/controller/ClientManagement.java
@@ -3,10 +3,6 @@ package com.cebem.controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-<<<<<<< HEAD
-=======
-import java.sql.Statement;
->>>>>>> 41ba6daac33284e8a1a029e820ae31da8abc41fa
 import java.util.ArrayList;
 import java.util.TimeZone;
 import java.sql.PreparedStatement;
@@ -103,7 +99,7 @@ public class ClientManagement {
 	}
 
 	// Method for adding clients to the DB
-	public static int addClient(Client c) throws ClassNotFoundException {
+	public static int addDB(Client c) throws ClassNotFoundException {
 		Connection con = null;
 		try {
 			con = getConnectionDB();
@@ -150,7 +146,6 @@ public class ClientManagement {
 
 		return 0;
 	}
-<<<<<<< HEAD
 
 	public void selectSingleDB(int id) {
 
@@ -159,51 +154,15 @@ public class ClientManagement {
 		for (Client e : arrayClients) {
 
 			if (e.equals(id)) {
-				boolean result = true;
+				
 				System.out.println(e); // Esto deberia ir en la interfaz
 				// guiclient
-				break;
+			
 
 			}
 		}
 
 	}
 
-=======
-	
-	// Method for getting the clients from the DB
-	public static ArrayList<Client> getClients(){
-		ArrayList<Client> clients = new ArrayList<Client>();
-		
-		Connection con = null;
-		String query = "SELECT * FROM Client";
-        ResultSet rs = null;
-		try {
-			con = getConnectionDB();
-			
-			Statement statement = con.createStatement();
-			rs = statement.executeQuery(query);
-			
-			while (rs.next()) {
-                Client c = new Client();
-                /*Retrieve one employee details 
-                and store it in employee object*/
-                c.setId(rs.getInt(1));
-                c.setName(rs.getString(2));
-                c.setSurname(rs.getString(3));
-                c.setTelephone(rs.getLong(4));
-                c.setEmail(rs.getString(5));
-                c.setAddress(rs.getString(6));
-                c.setPassword(rs.getString(7));
-                //add each employee to the list
-                clients.add(c);
-            }
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return clients;
-	}
->>>>>>> 41ba6daac33284e8a1a029e820ae31da8abc41fa
+
 }
