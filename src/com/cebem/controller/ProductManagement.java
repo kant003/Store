ec156cc31b3ce929722b0578336275d5956c6f0c
DAM <antvelo.avg@gmail.com>
2016-11-02 09:55:13 +0100

    reparando a Souto

--- src/com/cebem/controller/ProductManagement.java
+++ src/com/cebem/controller/ProductManagement.java
@@ -61,7 +61,7 @@ public class ProductManagement {
 				p.setRef(rs.getInt(3));
 				p.setPrice(rs.getDouble(4));
 				p.setIdprovider(rs.getInt(5));
-
+				Product.add(p);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -71,7 +71,35 @@ public class ProductManagement {
 		return p;
 	}
 
+	public static ArrayList<Product> getProducts() {
+		ArrayList<Product> products = new ArrayList<Product>();
 
+		String query = "SELECT * FROM Product";
+		ResultSet rs = null;
+		try {
+
+			Statement statement = con.createStatement();
+			rs = statement.executeQuery(query);
+
+			while (rs.next()) {
+				Product p = new Product();
+				/*
+				 * Retrieve one client details and store it in client object
+				 */
+				p.setId(rs.getInt(1));
+				p.setName(rs.getString(2));
+				p.setRef(rs.getInt(3));
+				p.setPrice(rs.getDouble(4));
+				p.setIdprovider(rs.getInt(5));
+				Product.add(p);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+		return products;
+	}
 
 }
 
--- src/com/cebem/controller/ProviderManagement.java
+++ src/com/cebem/controller/ProviderManagement.java
@@ -60,6 +60,7 @@ public class ProviderManagement {
 				pv.setName(rs.getString(2));
 				pv.setAddress(rs.getString(3));
 				pv.setEmail(rs.getString(4));
+				Provider.add(pv);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -67,6 +68,36 @@ public class ProviderManagement {
 			throw new SQLException();
 		}
 		return pv;
+	}
+	
+	public static ArrayList<Provider> getProviders() {
+		ArrayList<Product> providers = new ArrayList<Product>();
+
+		String query = "SELECT * FROM Provider";
+		ResultSet rs = null;
+		try {
+
+			Statement statement = con.createStatement();
+			rs = statement.executeQuery(query);
+
+			while (rs.next()) {
+				Provider pv = new Provider();
+				/*
+				 * Retrieve one client details and store it in client object
+				 */
+
+				pv.setId(rs.getInt(1));
+				pv.setName(rs.getString(2));
+				pv.setAddress(rs.getString(3));
+				pv.setEmail(rs.getString(4));
+				Provider.add(pv);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+		return providers;
 	}
 
 }
